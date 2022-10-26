86c729f8bca4631790060a04d0566f6d1d323a71
delizoderek <dwdelizo@gmail.com>
2022-10-25 22:02:27 -0700

    Added skill filter page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+	fab, 
+	faSquareJs, 
+	faCss3,
+	faHtml5,
+	faUnity,
+	faGitAlt,
+	faReact,
+} from '@fortawesome/free-brands-svg-icons'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import './App.css';
 
 function App() {
+	library.add(fab,)
   return (
     <div className="App">
       <NavBar />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,57 +1,81 @@
+import React, { useState } from 'react'
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+import Skill from './Skill'
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
-
+	const [ filters, setFilters ] = useState(() => new Set())
+	console.log(filters)
   const projects = [
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg1,
+			skill : ['git'],
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg2,
+			skill : ['C++'],
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg3,
+			skill : ['Javascript'],
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg1,
+			skill : ['C#'],
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg2,
+			skill : ['React'],
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg3,
+			skill : ['Express'],
     },
   ];
+
+	const onClick = (evt) => {
+		const filter = evt?.target?.name
+		const oldSet = new Set(filters)
+		if(oldSet.has(filter)){
+			oldSet.delete(filter)
+			setFilters(new Set(oldSet))
+		} else {
+			oldSet.add(filter)
+			setFilters(new Set(oldSet))
+		}
+	}
+
+	const filterFunction = ( item ) => {
+		return item.skill.some(skill => filters.has(skill))
+	}
 
   return (
     <section className="project" id="project">
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+              <div className="animate__animated animate__fadeIn">
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <div className="flex">
+									{projects.map(item => (<Skill key={item.skill} skill={item.skill} onClick={onClick}/>))}
+								</div>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
@@ -64,10 +88,20 @@ export const Projects = () => {
                       <Nav.Link eventKey="third">Tab 3</Nav.Link>
                     </Nav.Item>
                   </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                  <Tab.Content id="slideInUp" className="animate__animated animate__slideInUp">
                     <Tab.Pane eventKey="first">
                       <Row>
-                        {
+                        { filters.size > 0 ? 
+													projects.filter(filterFunction)
+													.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+												:
                           projects.map((project, index) => {
                             return (
                               <ProjectCard
@@ -87,8 +121,7 @@ export const Projects = () => {
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
-              </div>}
-            </TrackVisibility>
+              </div>
           </Col>
         </Row>
       </Container>
