5c6cc14645d923abfc2ef2c319cfa719e36ccc9b
delizoderek <dwdelizo@gmail.com>
2022-11-12 18:47:59 -0800

    Adding mobile styling for project cards

--- src/Components/containers/ProjectsContainer/index.jsx
+++ src/Components/containers/ProjectsContainer/index.jsx
@@ -1,23 +1,40 @@
-import React from "react";
-import { ProjectCard } from "../../elements";
+import React, { useEffect, useState } from "react";
+import DesktopContainer from "./DesktopContainer";
+import MobileContainer from "./MobileContainer";
 function ProjectsContainer({filters, items}) {
-
+  const [isMobile, setIsMobile] = useState(false);
 	const filterFunction = ( item ) => {
 		return item.skill.some(skill => filters.has(skill))
 	}
-	
+
+  //choose the screen size
+  const handleResize = () => {
+    if (window.innerWidth < 992) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // create an event listener
+  useEffect(() => {
+		handleResize()
+    window.addEventListener("resize", handleResize);
+  },[]);
+
+	let projectList = items
+	if(filters?.length > 0){
+		projectList = items.filter(filterFunction)
+	}
+
   return (
-    <div className="projects-container">
-      {filters.size > 0
-        ? items
-            .filter(filterFunction)
-            .map((project, index) => {
-              return <ProjectCard key={`ProjectCard-${index}`} {...project} />;
-            })
-        : items.map((project, index) => {
-            return <ProjectCard key={`ProjectCard-${index}`} {...project} />;
-          })}
-    </div>
+    <>
+      {isMobile ? (
+        <MobileContainer items={projectList} />
+				) : (
+        <DesktopContainer items={projectList} />
+      )}
+    </>
   );
 }
 
